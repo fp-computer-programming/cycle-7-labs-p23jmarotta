aa9206f5cc5cbebcec52895a46383e05d9456dae
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-11-03 10:53:45 -0400

    added 6-1

---
+++ lab_6-1.py
@@ -0,0 +1,11 @@
+# Creator JM 10/3/22
+
+my_arr = [1,2,3,4,5] # List
+
+print(my_arr[2]) # print 3rd item
+
+print(len(my_arr)) # Get the length
+
+print(my_arr * 3) # Repeat 3 times
+
+array = list('string') # Conver array to string
