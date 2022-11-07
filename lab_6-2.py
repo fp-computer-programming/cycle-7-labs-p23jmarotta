2e87534c3b035e0e54904850e838279d186ff358
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-11-07 12:00:51 -0500

    added 6-2 and 6-3

---
+++ lab_6-2.py
@@ -0,0 +1,14 @@
+# Creator JM 10/4/22
+
+my_row = ['sasser', 'preston'] # Define row
+
+my_row.append('joe') # Add my name
+
+my_row2 = my_row[:] # Set them equal to eachother
+
+print(my_row2) # print
+
+del my_row[1] # Delete index 1
+
+print(my_row) # The middle value is deleted
+# The two are not equal becuase we deleted after we defined my_row2
---
+++ lab_6-3.py
@@ -0,0 +1,14 @@
+# Creator JM 11/7/22
+
+colors = ['red', 'green', 'blue', 'yellow']
+
+colors.extend(['orange', 'purple', 'brown']) # Add these three to the end
+
+colors.append('pink') # Add pink
+
+colors.insert(3, 'black') # Insert at index 3
+
+colors2 = colors # Make a copy
+
+colors.remove('red') # Remove 'red'
+
