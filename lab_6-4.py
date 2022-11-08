f8f27941d44e0dafaa0753e239f0576e6dcc959a
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-11-08 13:18:12 -0500

    added lab 6-4

---
+++ lab_6-4.py
@@ -0,0 +1,13 @@
+# Creator JM 10/8/22
+
+subjects = ['biology', 'math', 'english'] # Define list of 3 subjects
+
+subjects.append('computer programming') # Add CP to the end of the list
+
+print(subjects.index('math')) # Find where math is 
+
+subjects.sort() # Sort in alphabetical order
+
+subjects2 = subjects.copy() # Copy to a new list
+
+subjects2.sort(reverse=True) # Reverse sort
