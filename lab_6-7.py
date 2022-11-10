10446f25e0621c220f4fb6d3c445291c1eaa7abf
Joseph Marotta <p23jmarotta@fairfieldprep.org>
2022-11-10 11:41:27 -0500

    revised 6-7 and 6-8

--- lab_6-7.py
+++ lab_6-7.py
@@ -1,9 +1,12 @@
 # Creator JM 10/9/22
 
-one = int(input('1st number: ')) # Define three numbers
-two = int(input('2nd number: '))
-three = int(input('3rd number: '))
-numbers = [one, two, three] # Put them in a list
-numbers_doubled = [one * 2, two * 2, three * 2] # Double each value
+numbers = [] # Define list
+
+print('Please provide three numbers:')
+
+for x in range(0,3): # this one doesn't say no loops!! :)
+    numbers.append(int(input())) # add 3 integers to list
+
+numbers_doubled = [numbers[0] * 2, numbers[1] * 2, numbers[2] * 2] # Double each value
 
 print(numbers_doubled) # print result
--- lab_6-8.py
+++ lab_6-8.py
@@ -6,7 +6,7 @@ three = int(input('3rd number: '))
 numbers = [one, two, three] # Put them in a list
 
 # Check if they are odd or even using modulus 2
-if one % 2 == 0 and two % 2 == 0 and three % 2 == 0:
+if one % 2 == 0 and two % 2 == 0 and three % 2 == 0: # This would be a great place for a loop
     print('even')
 elif one % 2 == 1 and two % 2 == 1 and three % 2 == 1:
     print('odd')
